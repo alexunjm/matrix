fcf14cb98ebcde55d86ef68a5a916194b75e522c
alex jaramillo <alexunjm@gmail.com>
2019-08-19 15:25:11 -0500

    operaciones de fila

--- gauss.js
+++ gauss.js
@@ -15,13 +15,140 @@ class Matrix {
     return this.arr[i-1][j-1];
   }
 
+  getFirstNonZeroColumnIndex(params = {}) {
+    let [i, j, val] = [0, 0, 0];
+    for (j = 0; j < this.arr[0].length; j++) {
+      for (i = 0; i < this.arr.length; i++) {
+        val = this.arr[i][j];
+        if (val != 0) {
+          i++;j++;
+          if (params.returnIJ) return {i, j};
+          return j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  swapRows(i1, i2) {
+    
+    this.arr = [...this.arr].map((row, index, sourceArr) => {
+      if ((index + 1) == i1) return sourceArr[i2 - 1];
+      else if ((index + 1) == i2) return sourceArr[i1 - 1];
+      return row;
+    });
+
+    return this;
+  }
+
+  rowScalar(params) {
+
+    // console.log(params);
+
+    if (!params.rowToOperate) {
+      for (let i = 0; i < this.arr.length; i++) {
+        const newParams = {...params, rowToOperate: {i: i+1}};
+        this.rowScalar(newParams);
+      }
+    } else {
+
+      const row = this.arr[params.rowToOperate.i - 1];
+      const scalar = params.scalar;
+      const fromCol = (params.rowToOperate.fromCol | 1) - 1;
+      let val;
+      for (let col = fromCol; col < row.length; col++) {
+        row[col] = row[col] * scalar;
+      }
+    }
+    
+  }
+
+  /**
+   * obtener ceros debajo de este elemento delantero, sumando 
+   * múltiplos adecuados del renglón superior a los renglones debajo de él
+   *
+   * @param {*} j
+   * @memberof Matrix
+   */
+  colOperations(j) {
+    let col = j-1, i;
+    let pivote = 0;
+    const params = {};
+    for (i = 0; i < this.arr.length; i++) {
+      pivote = this.arr[i][col];
+      if (pivote > 0) {
+        params.pivote = {
+          i, j: col
+        };
+        break;
+      }
+    }
+
+    let [val, scalar] = [0, 0];
+    for (++i; i < this.arr.length; i++) {
+      val = this.arr[i][col];
+      if (val > 0) {
+        scalar = -(pivote / val);
+        params.rowToOperate = { i };
+        params.scalar = scalar;
+        
+        this.matrixOps(fn.CONSTANTS.ROW_OPS.SUM_SCALAR_ROWS, params);
+      }
+    }
+    
+  }
+
+  matrixOps(op, params) {
+    console.log('operación de fila:', params);
+
+    switch (op) {
+      case fn.CONSTANTS.ROW_OPS.SUM_SCALAR_ROWS:
+        const pivoteRow = this.arr[params.pivote.i];
+        const row = this.arr[params.rowToOperate.i];
+        console.log('row before', row);
+        for (let col = params.pivote.j; col < row.length; col++) {
+          row[col] = (row[col] * params.scalar) + pivoteRow[col];
+        }
+        console.log('row after', row);
+        // fn.logMatrix(this);
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   showData() {
     console.table(this.arr);
+  }
+
+  equals(m) {
+    // console.log(JSON.stringify(this), JSON.stringify(m));
+    
+    let [i, j] = [0, 0];
+    for (j = 0; j < this.arr[0].length; j++) {
+      for (i = 0; i < this.arr.length; i++) {
+        if (m.arr[i][j] != this.arr[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  clone() {
+    return new Matrix([...this.arr].map(row => [...row]));
+    // return Object.assign(new Matrix([]), this);
   }
 }
 
 const fn = {
-  logMatrix: (m) => {console.table(m.arr)}
+  logMatrix: (m) => {console.table(m.arr)},
+  CONSTANTS: {
+    ROW_OPS: {
+      SUM_SCALAR_ROWS: 'SUM ROW OP'
+    }
+  }
 };
 
 module.exports = { ...fn, Matrix };
--- gauss.test.js
+++ gauss.test.js
@@ -16,7 +16,7 @@ const m = new Matrix([
   [1, 2, 3].map(val => val + 3),
   [1, 2, 3].map(val => val + 6)
 ]);
-logMatrix(m);
+// logMatrix(m);
 
 test("outbound error on get matrix element out of range", () => {
 
@@ -30,4 +30,146 @@ test("get matrix element", () => {
   expect(m.get(1, 2)).toBe(2);
   expect(m.get(2, 2)).toBe(5);
   expect(m.get(3, 3)).toBe(9);
+});
+
+test("find first no zero col", () => {
+  
+  expect(m.getFirstNonZeroColumnIndex()).toBe(1);
+  const m2 = new Matrix([
+    [0, 0, 3],
+    [0, 2, 3],
+    [0, 2, 3]
+  ]);
+  // logMatrix(m);
+  expect(m2.getFirstNonZeroColumnIndex()).toBe(2);
+  // expect(JSON.stringify(m2.getFirstNoZeroColumnIndex({returnIJ: true}))).toBe(JSON.stringify({i: 2, j: 2}));
+  expect(m2.getFirstNonZeroColumnIndex({returnIJ: true})).toEqual({i: 2, j: 2});
+});
+
+test("matrix equals method", () => {
+
+  expect(m.equals(m.clone())).toBeTruthy();
+  const m2 = new Matrix([
+    [0, 0, 3],
+    [0, 2, 3],
+    [0, 2, 3]
+  ]);
+  expect(m.equals(m2)).not.toBeTruthy();
+  const m3 = m2.clone();
+  m2.arr[0][0] = 5;
+  expect(m3.equals(m2)).not.toBeTruthy();
+});
+
+
+test("swap rows with a matrix", () => {
+  const m2 = new Matrix([
+    m.arr[1],
+    m.arr[0],
+    m.arr[2]
+  ]);
+  expect(m.swapRows(1, 2)).toEqual(m2);
+  // expect(m.swapRows(1, 2).equals(m2)).toBeTruthy();
+});
+
+test("swap rows operations", () => {
+
+  const m2 = new Matrix([
+    [0, 0, 3],
+    [0, 2, 3],
+    [0, 2, 3]
+  ]);
+
+  // 1. Ir a la primera columna no cero de izquierda a derecha.
+  const {i, j} = m2.getFirstNonZeroColumnIndex({returnIJ: true});
+  
+  // 2. Si la primera fila tiene un cero en esta columna, intercambiarlo con otra que no lo tenga.
+  if (i != 1) {
+    m2.swapRows(1, i);
+  }
+  expect(JSON.stringify(m2.arr)).toBe(JSON.stringify([
+    [0, 2, 3],
+    [0, 0, 3],
+    [0, 2, 3]
+  ]));
+});
+
+test("operate scalar row matrix", () => {
+
+  const m2 = new Matrix([
+    [0, 2, 3],
+    [0, 0, 5],
+    [0, 2, 3]
+  ]);
+
+  const params = {
+    scalar: 2,
+    rowToOperate: {i: 3}
+  }
+
+  const m3 = m2.clone();
+  m2.rowScalar(params);
+
+  expect(m2.get(3, 1)).toBe(m3.get(3, 1) * params.scalar);
+  expect(m2.get(3, 2)).toBe(m3.get(3, 2) * params.scalar);
+  expect(m2.get(3, 3)).toBe(m3.get(3, 3) * params.scalar);
+
+});
+
+test("operate scalar matrix", () => {
+
+  const m2 = new Matrix([
+    [0, 2, 3],
+    [0, 0, 5],
+    [0, 2, 3]
+  ]);
+
+  const params = {
+    scalar: 2
+  }
+
+  const m3 = m2.clone();
+  m2.rowScalar(params);
+
+  expect(m2.get(1, 1)).toBe(m3.get(1, 1) * params.scalar);
+  expect(m2.get(2, 2)).toBe(m3.get(2, 2) * params.scalar);
+  expect(m2.get(3, 3)).toBe(m3.get(3, 3) * params.scalar);
+
+});
+
+
+
+test("col operations to obtain zeros under first no zero value of matrix", () => {
+
+  const m2 = new Matrix([
+    [0, 0, 5],
+    [0, 2, 3],
+    [0, 2, 3]
+  ]);
+
+  // 1. Ir a la primera columna no cero de izquierda a derecha.
+  const {i, j} = m2.getFirstNonZeroColumnIndex({returnIJ: true});
+  
+  // 2. Si la primera fila tiene un cero en esta columna, intercambiarlo con otra que no lo tenga.
+  if (i != 1) {
+    m2.swapRows(1, i);
+  }
+    // [0, 2, 3],
+    // [0, 0, 5],
+    // [0, 2, 3]
+  /**
+   * 3. Luego, obtener ceros debajo de este elemento delantero, sumando 
+   * múltiplos adecuados del renglón superior a los renglones debajo de él.
+   *  */ 
+  m2.colOperations(j);
+
+  expect(m2.get(2, 2)).toBe(0);
+  expect(m2.get(3, 2)).toBe(0);
+  expect(m2.get(3, 3)).toBe(0);
+/*
+  expect(JSON.stringify(m2.arr)).toBe(JSON.stringify([
+    [0, 2, 3],
+    [0, 0, 5],
+    [0, 0, 0]
+  ]));*/
+  // 4. Cubrir el renglón superior y repetir el proceso anterior con la submatriz restante. Repetir con el resto de los renglones (en este punto la matriz se encuentra en forma escalonada).
 });
